c7185a950c8eb65dd194a4eaf9878bea1bc16bef
paul wisehart <paul@oldcode.org>
2019-02-21 13:53:39 -0500

    atoms

--- tut.erl
+++ tut.erl
@@ -1,6 +1,8 @@
 -module(tut).
 -export([double/1]).
 -export([fac/1]).
+-export([mult/2]).
+-export([convert/2]).
 
 double(X) ->
 	2 * X.
@@ -9,5 +11,13 @@ fac(1) ->
 	1;
 fac(N) ->
 	N * fac(N - 1).
+
+mult(X, Y) ->
+	X * Y.
+
+convert(M, inch) ->
+	M / 2.54;
+convert(N, centimeter) ->
+	N * 2.54.
 
 
